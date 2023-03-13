cbb0d6d561e9a810e45df157279b026108ad979b
fanoll <Fabian.Noll@googlemail.com>
2023-03-13 13:48:14 +0100

    created first basic struct for generalized Dependency output

--- internal/models/dependency.go
+++ internal/models/dependency.go
@@ -1,1 +1,17 @@
 package models
+
+type SBOM struct {
+	ProjectName  string
+	Languages    []string
+	Dependencies map[string][]Dependency
+}
+
+type Dependency struct {
+	ID         string
+	ImportName string
+	Version    string
+	Licenses   []string
+	Language   string
+	Source     string
+	TopLevel   bool
+}
