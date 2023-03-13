b29e238a13caed917745713e41e41e7e4934b360
fanoll <Fabian.Noll@googlemail.com>
2023-03-13 13:47:38 +0100

    created small and compact struct for syft file input

---
+++ internal/syft_convert.go
@@ -0,0 +1,21 @@
+package internal
+
+type Syft struct {
+	Artifacts []struct {
+		ID       string   `json:"id"`
+		Name     string   `json:"name"`
+		Version  string   `json:"version"`
+		Licenses []string `json:"licenses"`
+		Language string   `json:"language"`
+		Purl     string   `json:"purl"`
+	} `json:"artifacts"`
+	ArtifactRelationships []struct {
+		Parent string `json:"parent"`
+		Child  string `json:"child"`
+		Type   string `json:"type"`
+	} `json:"artifactRelationships"`
+	Source struct {
+		//used to identify source id in ArtifactRelations
+		ID string `json:"id"`
+	} `json:"source"`
+}
